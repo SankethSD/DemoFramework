7f6290dc4ddfa05775d633bb728f5b64bf1ae42d
Sanketh S D <sankethsd@Sankeths-MacBook-Air.local>
2024-12-18 11:01:45 +0530

    Initial commit

---
+++ DemoFrameworkTests/DemoFrameworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoFrameworkTests.swift
+//  DemoFrameworkTests
+//
+//  Created by Sanketh S D on 18/12/24.
+//
+
+import Testing
+@testable import DemoFramework
+
+struct DemoFrameworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
